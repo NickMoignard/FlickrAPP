9368f70549127a7511fae6497363b6f177a843d2
NickMoignard <nicklmoignard@gmail.com>
2018-04-30 15:45:15 +1000

    building model for application

--- FlickrRecent/Model/NetworkManager.swift
+++ FlickrRecent/Model/NetworkManager.swift
@@ -7,20 +7,69 @@
 //
 
 import Foundation
-//import Alamofire
+import Alamofire
+import SwiftyJSON
 
-fileprivate let apiKey = ""
+fileprivate let api_key = ""
 
-class NetworkManager {
+class FlickrNetworkManager {
     init() {
         
     }
     
-    func createSearchURL() {
+    
+    /// Get images from Flickr given a search term
+    func searchFlickr(_ searchTerm: String, completionHandler: @escaping (_ results: FlickrSearchResults, _ error: NSError?) -> Void) {
+     
+        
+        
+        
         
     }
     
-    func get(){
-        
+    fileprivate func escapeCharacters(_ term: String) -> String {
+        guard let escapedTerm = term.addingPercentEncoding(withAllowedCharacters: .alphanumerics) else {
+                return ""
+        }
+        return escapedTerm
     }
+    
+    fileprivate func createURLForSearchTerm(_ searchTerm: String, numResults: FlickrResultsPerPage = FlickrResultsPerPage.twenty) -> URL? {
+        var urlString = "https://api.flickr.com/services/rest/?"
+        
+        var params = baseParametersForFlickAPI(method: .search)
+        params["text"] = escapeCharacters(searchTerm)
+        params["per_page"] = FlickrResultsPerPage.twenty.rawValue
+        
+        for (key, val) in params {
+            urlString += "&\(key)=\(val)"
+        }
+        
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        
+        return url
+    }
+    
+    fileprivate func baseParametersForFlickAPI(method: FlickrAPIMethods) -> Parameters {
+        
+        let params: Parameters = [
+            "method" : method.rawValue,
+            "api_key": api_key,
+            "format" : "json",
+            "nojsoncallback": 1
+        ]
+        return params
+    }
+}
+
+enum FlickrAPIMethods: String {
+    case search = "flickr.photos.search"
+}
+
+enum FlickrResultsPerPage: Int {
+    case twenty = 20
+    case forty = 40
+    case hundred = 100
 }
