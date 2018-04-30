df556f6fa6bfe41f4deb68d340b3a7c0677e210d
NickMoignard <nicklmoignard@gmail.com>
2018-04-30 13:56:14 +1000

    init

---
+++ FlickrRecent/Model/FlickrSearchResults.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+
+
+/// Struct to wrap search string and search results from the string
+struct FlickrSearchResults {
+  let searchTerm : String
+  let searchResults : [FlickrPhoto]
+}
---
+++ FlickrRecent/Model/NetworkManager.swift
@@ -0,0 +1,26 @@
+//
+//  NetworkManager.swift
+//  FlickrRecent
+//
+//  Created by Alice Newman on 29/4/18.
+//  Copyright © 2018 Nicholas Moignard. All rights reserved.
+//
+
+import Foundation
+//import Alamofire
+
+fileprivate let apiKey = ""
+
+class NetworkManager {
+    init() {
+        
+    }
+    
+    func createSearchURL() {
+        
+    }
+    
+    func get(){
+        
+    }
+}
